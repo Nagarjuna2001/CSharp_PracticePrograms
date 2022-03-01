92261ab8af0c9b3122f55ee3fb64955e75b68572
Nagarjuna Kokatla <nagarjunakokatla@gmail.com>
2022-03-01 10:43:13 +0530

    Sample commit

---
+++ Delegates/EmployeesPromotion.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+//Code to print all the employees who are eligible to get promoted based on the no. of years of their working experience
+
+namespace Delegates
+{
+    class EmployeesPromotion
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> ListOfEmployees = new List<Employee>();
+
+            ListOfEmployees.Add(new Employee() { ID = 101, Name = "Arjun", Experience = 5 });
+            ListOfEmployees.Add(new Employee() { ID = 102, Name = "Varun", Experience = 4 });
+            ListOfEmployees.Add(new Employee() { ID = 103, Name = "Vishal", Experience = 7 });
+
+            IsPromotable isPromotable = new IsPromotable(CheckForPromotionEligibility);
+
+            Employee.CheckForPromotionEligibility(ListOfEmployees, isPromotable);
+        }
+
+        public static bool CheckForPromotionEligibility(Employee employee)
+        {
+            if (employee.Experience >= 5)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+    delegate bool IsPromotable(Employee employee);
+
+    class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Experience { get; set; }
+
+        public static void CheckForPromotionEligibility(List<Employee> EmployeeList, IsPromotable IsEligibleToPromote)
+        {
+            foreach (Employee employee in EmployeeList)
+            {
+                if (IsEligibleToPromote(employee))
+                {
+                    Console.WriteLine(employee.Name + " is eligible for promotion.");
+                }
+            }
+        }
+    }
+}
---
+++ WritingToConsole/HelloWorldProgram.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WritingToConsole
+{
+    class HelloWorldProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string LanguageToBeLearnt = "C#";
+            Console.Write("The Programming Language required for building applications in Dot Net framework is {0}", LanguageToBeLearnt);
+        }
+    }
+}
