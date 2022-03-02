502dc0d50e35bc4bad7fd816022abcdb09d5c720
Nagarjuna2001 <99009279+Nagarjuna2001@users.noreply.github.com>
2022-03-02 19:52:19 +0530

    Add files via upload

---
+++ MethodHiding/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MethodHiding
+{
+    class Base
+    {
+        public void print()
+        {
+            Console.WriteLine("Base method is called");
+        }
+    }
+
+    class Derived : Base
+    {
+        public new void print()
+        {
+            Console.WriteLine("Derived method is called");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Base obj = new Derived();
+            obj.print();
+
+        }
+    }
+}
